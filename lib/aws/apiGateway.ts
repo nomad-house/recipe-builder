22ff396f0e5e28f1ec562ae917bbb6b4ab4a66f4
Matthew Keil <malibuss67@gmail.com>
2020-10-18 21:12:30 -0400

    added core stack

--- lib/aws/apiGateway.ts
+++ lib/aws/apiGateway.ts
@@ -1,15 +1,13 @@
-import { APIGateway, IAM } from "aws-sdk";
+import { APIGateway } from "aws-sdk";
 
-const iam = new IAM({ region: process.env.REGION });
 const apiGateway = new APIGateway({ region: process.env.REGION });
 
 export const apiGatewayAccountExists = async () => {
   try {
     const { cloudwatchRoleArn } = await apiGateway.getAccount().promise();
-    const RoleName = cloudwatchRoleArn.split("/")[1];
-    await iam.getRole({ RoleName }).promise();
-    return true;
+    return cloudwatchRoleArn;
   } catch (err) {
-    if (err.code === "NoSuchEntity") return false;
+    if (err.code === "NoSuchEntity") return;
+    throw err;
   }
 };
--- lib/cdk/coreStack.ts
+++ lib/cdk/coreStack.ts
@@ -1,10 +1,20 @@
-import {ArnPrincipal, Effect, Policy, PolicyStatement, Role, ServicePrincipal} from '@aws-cdk/aws-iam';
-import {HostedZone} from '@aws-cdk/aws-route53';
-import {Certificate, CertificateValidation} from '@aws-cdk/aws-certificatemanager';
-import {Construct, Stack, StackProps} from '@aws-cdk/core';
+import {
+  ArnPrincipal,
+  Effect,
+  Policy,
+  PolicyStatement,
+  Role,
+  ServicePrincipal,
+  PolicyDocument
+} from "@aws-cdk/aws-iam";
+import { HostedZone } from "@aws-cdk/aws-route53";
+import { CfnAccount } from "@aws-cdk/aws-apigateway";
+import { Certificate, CertificateValidation } from "@aws-cdk/aws-certificatemanager";
+import { Construct, Stack, StackProps } from "@aws-cdk/core";
 
 export interface CoreStackParams extends StackProps {
   domainName: string;
+  cloudWatchRoleArn?: string;
 }
 
 export class CoreStack extends Stack {
@@ -12,15 +22,34 @@ export class CoreStack extends Stack {
   public certificate: Certificate;
 
   constructor(scope: Construct, id: string, params: CoreStackParams) {
-    const {domainName} = params;
+    const { domainName, cloudWatchRoleArn } = params;
     super(scope, id, params);
 
-    // apigateway account for loggingc
+    const cloudWatchRole = cloudWatchRoleArn
+      ? Role.fromRoleArn(this, "ApiGatgewayAccountRole", cloudWatchRoleArn)
+      : new Role(this, "ApiGatgewayAccountRole", {
+          assumedBy: new ServicePrincipal("apigateway.amazonaws.com")
+        });
+    cloudWatchRole.attachInlinePolicy(
+      new Policy(this, "ApiGatewayLoggingPolicy", {
+        policyName: "api-gateway-logging-policy",
+        statements: [
+          new PolicyStatement({
+            effect: Effect.ALLOW,
+            actions: ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"],
+            resources: ["*"]
+          })
+        ]
+      })
+    );
+    if (!cloudWatchRoleArn) {
+      new CfnAccount(this, "ApiGatewayAccount", { cloudWatchRoleArn: cloudWatchRole.roleArn });
+    }
 
-    this.hostedZone = new HostedZone(this, 'HostedZone', {
+    this.hostedZone = new HostedZone(this, "HostedZone", {
       zoneName: domainName
     });
-    this.certificate = new Certificate(this, 'Certificate', {
+    this.certificate = new Certificate(this, "Certificate", {
       domainName,
       validation: CertificateValidation.fromDns(this.hostedZone)
     });
