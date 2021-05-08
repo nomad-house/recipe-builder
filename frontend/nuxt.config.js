50d39a848f4f728fc91ba0ba559f5fe53f11b669
matthewkeil <matt.keil@voicefoundry.com>
2021-05-08 01:34:26 -0400

    building dist correctly

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -72,5 +72,12 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    babel:{
+      plugins: [
+        ['@babel/plugin-proposal-private-methods', { loose: true }]
+      ]
+    }
+  }
+
 }
