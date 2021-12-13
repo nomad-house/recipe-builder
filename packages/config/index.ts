dd99cec551d10dfa7f0ffe36eff386b5489df104
Matthew Keil <me@matthewkeil.com>
2021-12-13 05:18:57 -0500

    synths

--- packages/config/index.ts
+++ packages/config/index.ts
@@ -1,6 +1,5 @@
+import { resolve } from "path";
 import { getConfig as GET_CONFIG, FullStackConstructProps } from "full-stack-pattern";
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require("dotenv").config();
 
 const staticProps = {
   client: "co", // CODEified Org
@@ -22,19 +21,22 @@ interface Stage extends FullStackConstructProps {
 // const GITHUB_CLIENT_SECRET = `${process.env.GITHUB_CLIENT_SECRET}`;
 // const ORIGIN = process.env.ORIGIN ?? "http://localhost:3001";
 
-const stages: Stage[] = [
-  {
-    branch: "master",
-    /* eslint-disable @typescript-eslint/no-non-null-assertion */
-    profile: process.env.PROFILE!,
-    env: {
-      account: process.env.ACCOUNT_ID!,
-      region: process.env.REGION!
-    }
-    /* eslint-enable @typescript-eslint/no-non-null-assertion */
-  }
-];
-
 export function getConfig(branch?: string) {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require("dotenv").config({ path: resolve(__dirname, "..", "..", "..", ".env") });
+
+  const stages: Stage[] = [
+    {
+      branch: "master",
+      /* eslint-disable @typescript-eslint/no-non-null-assertion */
+      profile: process.env.PROFILE!,
+      env: {
+        account: process.env.ACCOUNT_ID!,
+        region: process.env.REGION!
+        /* eslint-enable @typescript-eslint/no-non-null-assertion */
+      }
+    }
+  ];
+
   return GET_CONFIG(stages, staticProps)(branch);
 }
---
+++ packages/serverless/bin/buildLayer.ts
@@ -0,0 +1,28 @@
+import { resolve } from "path";
+import { writeFile, mkdir } from "fs/promises";
+import { exec } from "@codeified/utils";
+
+import originalPackageJson from "../package.json";
+
+const OUTPUT_DIR = resolve(__dirname, "..", "dist", "layer", "nodejs");
+const PACKAGE_JSON = resolve(OUTPUT_DIR, "package.json");
+
+async function buildPackageJson(): Promise<void> {
+  const newPackageJson: any = { ...originalPackageJson };
+  delete (newPackageJson as any).scripts;
+  delete (newPackageJson as any).devDependencies;
+  delete (newPackageJson as any).jest;
+  newPackageJson.name = `${newPackageJson.name}-layer`;
+  newPackageJson.description = `Lambda layer for ${newPackageJson.name}`;
+  await writeFile(PACKAGE_JSON, JSON.stringify(newPackageJson, undefined, 2));
+}
+
+export async function buildLayer(): Promise<void> {
+  await mkdir(OUTPUT_DIR, { recursive: true });
+  await buildPackageJson();
+  await exec(`cd ${OUTPUT_DIR} && npm i --only=prod --no-package-lock`);
+}
+
+if (require.main === module) {
+  buildLayer();
+}
