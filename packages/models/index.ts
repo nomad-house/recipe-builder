d7b2f019cc3552c09d1a36564b7cdebbef143ec3
Matthew Keil <me@matthewkeil.com>
2022-04-13 19:41:12 -0400

    working on data types

--- packages/models/index.ts
+++ packages/models/index.ts
@@ -1,1 +1,20 @@
-export class Recipe {}
+import { Entity, AutoGenerateAttribute, AUTO_GENERATE_ATTRIBUTE_STRATEGY } from "@typedorm/common";
+import { Field, ID, ObjectType } from "type-graphql";
+import { IsString } from "class-validator";
+
+@ObjectType()
+@Entity({
+  name: "recipes",
+  primaryKey: {
+    partitionKey: "#{{id}}",
+    sortKey: "#{{id}}"
+  }
+})
+export class Recipe {
+  @IsString()
+  @Field(() => ID)
+  @AutoGenerateAttribute({
+    strategy: AUTO_GENERATE_ATTRIBUTE_STRATEGY.EPOCH_DATE
+  })
+  id!: string;
+}
