ce16a22c4c8db8889bb79ed377dfe8471b963bad
Matthew Keil <me@matthewkeil.com>
2022-04-13 21:10:02 -0400

    worked through repository pattern

--- packages/models/index.ts
+++ packages/models/index.ts
@@ -1,6 +1,35 @@
-import { Entity, AutoGenerateAttribute, AUTO_GENERATE_ATTRIBUTE_STRATEGY } from "@typedorm/common";
-import { Field, ID, ObjectType } from "type-graphql";
 import { IsString } from "class-validator";
+import { Field, ID, ObjectType } from "type-graphql";
+import { EntityManager, getEntityManager } from "@typedorm/core";
+import { Entity, AutoGenerateAttribute, AUTO_GENERATE_ATTRIBUTE_STRATEGY } from "@typedorm/common";
+
+export class Repository<T> {
+  private entityManager = getEntityManager();
+
+  constructor(private ctr: new (...args: any[]) => T) {}
+
+  // public static findOne(props: Partial<Recipe>) {
+  //   return entityManger.findOne(Recipe, {
+  //     id: response.id,
+  //     status: "onboarding",
+  //     active: true
+  //   });
+  // }
+
+  public findById(id: string) {
+    return this.entityManager.findOne(this.ctr, { id });
+  }
+
+  // public static delete(idOrRecipe: string | Recipe) {
+  //   return RecipeRepository.entityManager.delete(Recipe, {
+  //     id: typeof idOrRecipe === "string" ? idOrRecipe : idOrRecipe.id
+  //   });
+  // }
+
+  // public static create(recipe: Recipe) {
+  //   return RecipeRepository.entityManager.create(recipe);
+  // }
+}
 
 @ObjectType()
 @Entity({
@@ -18,3 +47,7 @@ export class Recipe {
   })
   id!: string;
 }
+
+const Recipes = new Repository<Recipe>(Recipe);
+
+Recipes.findById("1");
