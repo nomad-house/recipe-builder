651c260723d2425115f8ac8630b5814c5ebb4f7e
Matthew Keil <me@matthewkeil.com>
2022-04-14 08:25:23 -0400

    setting up server

--- packages/models/src/Recipe.ts
+++ packages/models/src/Recipe.ts
@@ -4,6 +4,13 @@ import { EntityManager, getEntityManager } from "@typedorm/core";
 import { Entity, AutoGenerateAttribute, AUTO_GENERATE_ATTRIBUTE_STRATEGY } from "@typedorm/common";
 import { Repository } from "./Repository";
 
+interface Ingredient {}
+interface Step {}
+class RecipeVersion {
+  ingredients!: Ingredient[];
+  steps!: Step[];
+}
+
 @ObjectType()
 @Entity({
   name: "recipes",
@@ -19,4 +26,15 @@ export class Recipe {
     strategy: AUTO_GENERATE_ATTRIBUTE_STRATEGY.EPOCH_DATE
   })
   id!: string;
+
+  @Field(() => RecipeVersion)
+  versions!: RecipeVersion[];
+
+  get ingredients() {
+    return this.versions[0].ingredients;
+  }
+
+  get steps() {
+    return this.versions[0].steps;
+  }
 }
