0ac9d01a3c28a198887a0c82299ac000ca5f8e58
matthewkeil <me@matthewkeil.com>
2022-08-24 20:36:02 -0600

    add storybook and frontend with react 18

--- packages/frontend/src/Components/RecipeImage/RecipeImage.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./recipeImage.css";
-
-export default function RecipeImage({ image, recipeName }) {
-  return <img className="Recipe__Icon" src={image} alt={recipeName} />;
-}
--- packages/frontend/src/Components/RouteError/RouteError.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function RouteError() {
-  return (
-    <div>Route not found</div>
-  )
-}
-
-export default RouteError
